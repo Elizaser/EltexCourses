43fe8e0f00a98021f7697ad3037be8ea6f7aa431
Elizaser <liza.gluhowerowa@gmail.com>
2023-06-20 21:34:32 +0700

    bringing to google style

--- ex1BitOperations/bitOperations.c
+++ ex1BitOperations/bitOperations.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
-void printBytes(int);
-void replacementThirdByte(int*, int, unsigned char);
+void PrintBytes(int);
+void ReplacementThirdByte(int*, int, unsigned char);
 
 int main() 
 {
 	int a = 0xDDCCBBAA;
 	
-	printBytes(a);
-	replacementByte(&a, 3, 0x15);
-	printBytes(a);
+	PrintBytes(a);
+	ReplacementByte(&a, 3, 0x15);
+	PrintBytes(a);
 
 	return 0;
 }
 
-void printBytes(int a) {
+void PrintBytes(int a) {
 	int countBytes = 4;
 	int countBits = 8;
 	unsigned char c;
-	printf("\n") ;
+	
+	printf("\n");
 	for(int i = 0; i <  countBytes; i++){
 		c = (a&0xFF);
 		printf("bytes of %d :%x\n", i + 1, c);
@@ -25,17 +26,16 @@ void printBytes(int a) {
 	}
 }
 
-void replacementByte(int *a, int byteNumber, unsigned char byte) {
+int ReplacementByte(int *a, int byteNumber, unsigned char byte) {
 	int countBytes = 4;
 	int countBits = 8;
-	unsigned char c;
 
 	int mask2 = byte << (countBits * (byteNumber - 1));
 	int mask1 = 0xFFFFFFFF;
 
-	if(byteNumber > countBytes) {
+	if(byteNumber > countBytes || byteNumber < 0) {
 		printf("Такого байта в числе нет\n");
-		return;
+		return -1;
 	}
 
 	switch(byteNumber){
@@ -52,5 +52,7 @@ void replacementByte(int *a, int byteNumber, unsigned char byte) {
 			mask1 = 0x00FFFFFF;
 	}	
 
-	*a = *a&mask1 | mask2;  
+	*a = *a & mask1 | mask2;
+
+	return 0;  
 }
