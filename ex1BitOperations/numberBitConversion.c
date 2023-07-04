e2e16d26c020703955a53a27124cea1b171bee89
Elizaser <liza.gluhowerowa@gmail.com>
2023-07-04 16:37:39 +0700

    add exercize3

--- ex1BitOperations/numberBitConversion.c
+++ ex1BitOperations/numberBitConversion.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-void PrintBytes(int);
-void ReplacementThirdByte(int*, int, unsigned char);
 
-int main() 
-{
+#define kCountBytes  4
+#define kCountBits  8
+
+void PrintBytes(int);
+int ReplacementByte(int*, int, unsigned char);
+
+int main()  {
 	int a = 0xDDCCBBAA;
 	
 	PrintBytes(a);
@@ -14,26 +17,21 @@ int main()
 }
 
 void PrintBytes(int a) {
-	int countBytes = 4;
-	int countBits = 8;
 	unsigned char c;
 	
 	printf("\n");
-	for(int i = 0; i <  countBytes; i++){
+	for(int i = 0; i <  kCountBytes; i++){
 		c = (a&0xFF);
 		printf("bytes of %d :%x\n", i + 1, c);
-		a = a>>countBits;
+		a = a>>kCountBits;
 	}
 }
 
 int ReplacementByte(int *a, int byteNumber, unsigned char byte) {
-	int countBytes = 4;
-	int countBits = 8;
-
-	int mask2 = byte << (countBits * (byteNumber - 1));
+	int mask2 = byte << (kCountBits * (byteNumber - 1));
 	int mask1 = 0xFFFFFFFF;
 
-	if(byteNumber > countBytes || byteNumber < 0) {
+	if(byteNumber > kCountBytes || byteNumber < 0) {
 		printf("Такого байта в числе нет\n");
 		return -1;
 	}
